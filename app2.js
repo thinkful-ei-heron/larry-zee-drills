258412d336fd290d941badf63087c7587bfe11ac
Zee Celest <zeecelest@gmail.com>
2019-09-18 14:20:48 -0400

    added enroll drill

---
+++ app2.js
@@ -0,0 +1,24 @@
+'use strict';
+//iterating through objects drills
+//Make student reports drill
+function makeStudentsReport(data) {
+  let results = [];
+  for (let i = 0; i < data.length; i++) {
+    let item = data[i];
+    results.push(`${item.name}: ${item.grade}`);
+  }
+  return results;
+}
+
+//enroll in summer school drill
+function enrollInSummerSchool(students) {
+  let results = [];
+  for (let i = 0; i < students.length; i++) {
+    results.push({
+      name: students[i].name,
+      status: 'In Summer school',
+      course: students[i].course
+    });
+  }
+  return results;
+}
