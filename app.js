43768786f1a8178cb022eeba3c391ea963ffd76f
Zee Celest <zeecelest@gmail.com>
2019-09-18 12:34:27 -0400

    object updater drill

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello() {
+      return 'hello';
+    }
+  };
+}
+console.log(createMyObject());
+
+//object updater
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
+//self-reference
